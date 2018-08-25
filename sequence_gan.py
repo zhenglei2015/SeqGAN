500635dd2cd68a614912203f6f6f98b4b0240440
zhenglei2015 <245767493@qq.com>
2018-08-25 06:52:54 +0000

    aa

--- sequence_gan.py
+++ sequence_gan.py
@@ -106,7 +106,7 @@ def main():
     sess.run(tf.global_variables_initializer())
 
     # First, use the oracle model to provide the positive examples, which are sampled from the oracle data distribution
-    generate_samples(sess, target_lstm, BATCH_SIZE, generated_num, positive_file)
+    #generate_samples(sess, target_lstm, BATCH_SIZE, generated_num, positive_file)
     gen_data_loader.create_batches(positive_file)
 
     log = open('save/experiment-log.txt', 'w')
